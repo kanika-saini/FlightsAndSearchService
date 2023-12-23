87a7bc4de660313a4ad567a725c432d136b21690
Kanika saini <kanikasaini798@gmail.com>
2023-12-23 14:02:43 +0530

    Added routers to complete create api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
+const ApiRoutes = require("./routes/index");
 const setupAndStartServer = async () => {
   //creating express object
   const app = express();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
+  app.use("/api", ApiRoutes);
   app.listen(PORT, () => {
     console.log(`started server at ${PORT}`);
   });
