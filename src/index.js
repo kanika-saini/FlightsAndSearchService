1bede2c768ccaab9bdd50e4167e2a61b66c0cba9
Kanika saini <kanikasaini798@gmail.com>
2023-12-26 14:52:37 +0530

    Added airlanes db and seeded data using seed file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
 const ApiRoutes = require("./routes/index");
 const db = require("./models/index");
-const { City, Airport } = require("./models/index");
+const { Airplane } = require("./models/index");
+const airplane = require("./models/airplane");
 const setupAndStartServer = async () => {
   //creating express object
   const app = express();
