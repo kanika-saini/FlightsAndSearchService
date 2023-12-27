75d729dce5177ecbccd7c62df0132d8d29c54234
Kanika saini <kanikasaini798@gmail.com>
2023-12-27 23:22:54 +0530

    added flight create-request middleware

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { FlightMiddlewares } = require("../../middlewares/index");
 const CityController = require("../../controllers/city-controller");
 const FlightController = require("../../controllers/flight-controller");
 const AirportController = require("../../controllers/airport-controller");
@@ -11,7 +12,12 @@ router.get("/city", CityController.getAll);
 router.patch("/city/:id", CityController.update);
 
 // routes for flight
-router.post("/flights", FlightController.create);
+router.post(
+  "/flights",
+  FlightMiddlewares.validateCreateFlight,
+  FlightController.create
+);
+
 router.get("/flights", FlightController.getAll);
 
 // airport routes
